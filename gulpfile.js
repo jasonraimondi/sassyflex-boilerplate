93117491c33ba566df12d5c351578b637b99cd22
Jason Raimondi <jason@raimondi.us>
2015-05-14 15:30:14 -0400

    switch from laravel elixir gulp to a traditional gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,87 @@ var paths = {
  'BOWER': './vendor/bower_components/'
 }
 
-var elixir = require('laravel-elixir');
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+var gulp = require('gulp'),
+    gulpif = require('gulp-if'), // if --production flag
+    argv = require('yargs').argv, // allow for command line arguments
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del'); // clean paths.DESTINATION before running
 
-
-gulp.task('imagemin', function () {
-return gulp.src(paths.SOURCE + 'images/**/*.{png,jpg,gif}')
-    .pipe(imagemin({
-     progressive: true,
-     svgoPlugins: [{removeViewBox: false}],
-     use: [pngquant()]
-    }))
-    .pipe(gulp.dest(paths.DESTINATION + 'images'));
+gulp.task('styles', function() {
+  return gulp.src(paths.SOURCE + 'sass/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        style: 'expanded',
+        includePaths : [
+               paths.BOWER + 'normalize-scss',
+               paths.BOWER + 'bourbon/app/assets/stylesheets',
+               paths.BOWER + 'neat/app/assets/stylesheets',
+               paths.BOWER + 'sassyflex/sass'
+           ]
+    }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulpif(argv.production, minifycss()))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(paths.DESTINATION + 'css'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
+gulp.task('scripts', function() {
 
-elixir(function(mix) {
+  return gulp.src(paths.SOURCE + 'js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(paths.DESTINATION + 'js'))
+    .pipe(gulpif(argv.production, uglify()))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(paths.DESTINATION + 'js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
 
-    mix.sass('style.scss', paths.DESTINATION + 'css', {
-        includePaths : [
-            paths.BOWER + 'sassyflex/sass',
-            paths.BOWER + 'bourbon/app/assets/stylesheets',
-            paths.BOWER + 'neat/app/assets/stylesheets',
-        ]}
-    );
+});
 
-    mix.styles(paths.BOWER + 'normalize.css/normalize.css', paths.DESTINATION + 'css/normalize.css', './');
+gulp.task('bower-scripts', function() {
+  return gulp.src([
+        paths.BOWER + 'jquery/dist/jquery.js',
+        paths.BOWER + 'modernizr/modernizr.js'
+    ])
+    .pipe(sourcemaps.init())
+    // .pipe(jshint('.jshintrc'))
+    // .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest(paths.DESTINATION + 'js'))
+    .pipe(gulpif(argv.production, uglify()))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(paths.DESTINATION + 'js'))
+    .pipe(notify({ message: 'Bower Scripts task complete' }));
+});
 
-    mix.scripts(paths.BOWER + 'jquery/dist/jquery.js', paths.DESTINATION + 'js/jquery.js', './');
-    mix.scripts(paths.BOWER + 'modernizr/modernizr.js', paths.DESTINATION + 'js/modernizr.js', './');
+gulp.task('images', function() {
+  return gulp.src(paths.SOURCE + 'images/**/*')
+    .pipe(cache(imagemin({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })))
+    .pipe(gulp.dest(paths.DESTINATION + 'images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
 
-    mix.copy(paths.SOURCE + 'images', paths.DESTINATION + 'images');
+gulp.task('clean', function(cb) {
+    del(paths.DESTINATION, cb)
+});
 
-    mix.scriptsIn(paths.SOURCE + 'js/app', paths.DESTINATION + 'js/main.js');
-
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'bower-scripts', 'images');
 });
